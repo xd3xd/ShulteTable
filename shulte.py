0c4204693a885f4277af2238a8b994e34f9c044c
xd3xd <belosnejka2005@gmail.com>
2024-11-18 16:26:46 +0500

    Обновление ввода размеров таблицы и их валидация

--- shulte.py
+++ shulte.py
@@ -3,28 +3,42 @@ from tkinter import messagebox
 import random
 import time
 
+def val(n):
+    while True:
+        try:
+            n = int(n)
+            if int(n)>=1:
+                return int(n)
+            else:
+                n = input("Введите натуральное число: ")
+        except:
+            n = input("Вы ввели не число. Введите снова: ")
+
 class ShulteTable:
     def __init__(self, master):
         self.master = master
         self.master.title("Таблица Шульте")
 
-        self.size = 5
-        self.numbers = list(range(1, self.size**2 + 1))
+        self.rows = n
+        self.columns = m
+        self.numbers = list(range(1, n*m + 1))
         random.shuffle(self.numbers)
 
         self.start_time = None
-        self.last_number = self.size**2
+        self.last_number = int(self.rows) * int(self.columns)
         self.current_number = 1
 
         self.buttons = []
-        for i in range(self.size):
+        k = 0
+        for i in range(int(self.rows)):
             row_buttons = []
-            for j in range(self.size):
-                number = self.numbers[i * self.size + j]
+            for j in range(int(self.columns)):
+                number = self.numbers[k]
                 button = tk.Button(master, text=str(number), width=5, height=2, command=lambda num=number: self.on_click(num))
                 button.grid(row=i, column=j, padx=5, pady=5)
                 row_buttons.append(button)
-            self.buttons.append(row_buttons)
+                k+=1
+        self.buttons.append(row_buttons)
 
     def on_click(self, number):
         if number == 1 and self.start_time is None:
@@ -39,6 +53,11 @@ class ShulteTable:
             self.current_number += 1
         else:
             print(f"Ошибка! Вы должны нажать {self.current_number}.")
+
+n = input("Введите размер поля по ширине: ")
+n = val(n)
+m = input("Введите размер поля по высоте: ")
+m = val(m)
 
 mainwindow = tk.Tk()
 shulte_app = ShulteTable(mainwindow)
