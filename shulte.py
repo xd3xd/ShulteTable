b9d31585092b3f8e3b103c0e27514e5d46906823
xd3xd <belosnejka2005@gmail.com>
2024-12-26 19:45:24 +0500

    Создание секундомера, перенос кнопки завершения программы

--- shulte.py
+++ shulte.py
@@ -2,9 +2,13 @@ import tkinter as tk
 from tkinter import simpledialog, messagebox
 import random
 import time
+from datetime import datetime
 import sys
+import threading
 
-
+start_time = 0
+timer_running = False
+k = 0
 
 def get_two_variables():
     def submit():
@@ -17,9 +21,7 @@ def get_two_variables():
                 if (var1 > 0) and (var2 > 0):
                     root.quit()
                     button_submit.destroy()
-                    button_exit.pack(pady = 10)
                     label_error1.pack(pady = 3)
-                    label_error1["text"] = f"Для изменения размеров таблицы\nШульте, перезапустите программу"
                     return var1, var2
                 else:
                     label_error1.pack(pady = 5)
@@ -31,8 +33,6 @@ def get_two_variables():
                 label_error1["text"] = "Введите числовые значения"
                 root.update()
                 pass
-    def exit():
-        sys.exit()
 
     root = tk.Tk()
     root.title("Введите размеры таблицы")
@@ -52,22 +52,59 @@ def get_two_variables():
     button_submit = tk.Button(root, text="Подтвердить", command=submit)
     button_submit.pack(pady=20)
 
-    button_exit = tk.Button(root, text="Выйти", command=exit)
-
     label_error1 = tk.Label(root, text="")
 
     root.mainloop()
 
     return int(entry_var1.get()), int(entry_var2.get())
 
+def info():
+    global k, timer_running, start_game, end_game
+    if k == 0:
+        info_root = tk.Tk()
+        info_root.title("Информация")
+        info_root.resizable(True,True)
+        info_root.geometry("300x100+800+100")
+        k+=1
 
+        def start_game():
+            global start_time, timer_running
+            start_time = time.time()
+            timer_running = True
+            if timer_running == True:
+                update_timer()
+        
+        def end_game():
+            global timer_running
+            timer_running = False
+            elapsed_time = time.time() - start_time
+            timer_label["text"]=(f"Вы закончили! Время: {elapsed_time:.2f} секунд")
+
+        def update_timer():
+            if timer_running:
+                elapsed_time = time.time() - start_time
+                timer_label.config(text=f"Время: {elapsed_time:.2f} секунд")
+                info_root.after(100, update_timer)
+            else:
+                end_game()
+
+        def exit():
+            sys.exit()
+
+        timer_label = tk.Label(info_root, text="Время: 0.00 секунд", font=("Times New Roman", 14, "bold"))
+        timer_label.pack(pady = 10)
+
+        button_exit = tk.Button(info_root, text="Выйти", command=exit)
+        button_exit.pack(pady = 10)
 
 class ShulteTable:
     def __init__(self, master):
+        global label,info_root
         self.master = master
         self.master.title("Таблица Шульте")
         self.master.resizable(False,False)
         self.master.geometry("+400+100")
+    
 
         self.rows = n
         self.columns = m
@@ -91,16 +128,19 @@ class ShulteTable:
                 k += 1
 
     def on_click(self, number):
+        global start_game, end_game
+
         if number == 1 and self.start_time is None:
-            self.start_time = time.time()
-            print("Вы начали! Время запущено.")
+            start_game()
         
         if number == self.last_number and self.current_number == self.last_number:
+            label = self.labels[number]
             label.config(fg = 'white')
             label.unbind("<Button-1>")
             if self.start_time is not None:
-                elapsed_time = time.time() - self.start_time
-                print(f"Вы закончили! Время: {elapsed_time:.2f} секунд.")
+                self.start_time = None
+                end_game()
+
         elif number == self.current_number:
             self.current_number += 1
             label = self.labels[number]
@@ -114,4 +154,5 @@ class ShulteTable:
 n, m = get_two_variables()
 mainwindow = tk.Tk()
 shulte_app = ShulteTable(mainwindow)
+info_w = info()
 mainwindow.mainloop()
